d2cfac817c796c1addd2b366e54ec2fef8d1e44a
Anna Olson <olsona@cs.uchicago.edu>
2013-09-24 21:40:39 -0500

    Bug

--- OlsonUtilities.py
+++ OlsonUtilities.py
@@ -276,11 +276,11 @@ def csv2MediansAll(inFile, outFile):
     fout.close()
 
 
-def csv2MediansTop(inFile, outFile, numContigs, numTop):
+def csv2MediansTop(inFile, outFile, numTop):
     import numpy
     mat = numpy.genfromtxt(inFile, dtype=numpy.float32, delimiter=" ")
     fout = open(outFile, 'w')
-    for r in range(len(mat)/(numTop*numContigs)):
+    for r in range(len(mat)/numTop):
         segment = mat[r*numTop:(r+1)*numTop]
         fout.write("{!s}\n".format(numpy.median(segment,axis=0)[2]))
     fout.close()
