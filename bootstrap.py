7477f4c5d3043ac878f2013655aadb32270ef6bb
Anna Olson <olsona@cs.uchicago.edu>
2014-01-14 15:59:58 -0600

    bug

--- bootstrap.py
+++ bootstrap.py
@@ -91,7 +91,10 @@ def main(argv):
                 s = 0
                 while ln:
                     if ln[0] == '>': # deal with name lines
-                        m = re.search('[A-Za-z]',ln).group()[0]
+                        m = re.search('[A-Za-z]',ln)
+                        print m
+                        print m.group()
+                        print m.group()[0]
                         if s == 0: # start file
                             fN.write(ln.rstrip()[m:]+'\t')
                             s = 1
