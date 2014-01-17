1ef948ba67267e874ffff1f53a2e483311dea0e0
Anna Olson <olsona@cs.uchicago.edu>
2014-01-17 10:53:17 -0600

    Test test test

--- bootstrapConstants.py
+++ bootstrapConstants.py
@@ -6,9 +6,8 @@ usageString = 'bootstrap.py -i <input file> -o <output file> -p <RAIphy path> '\
 bigUsageString = '-i, --ifile\t\tInput metagenomics FASTA file\n'+\
                 '-o, --ofile\t\tOutput file\n'+\
                 '-c, --schedule\tCooling schedule (optional)\n'+\
-                '\t\t\t  format: [5,2,1]\n'+\
-                '\t\t\t  [1,2,5] implies the cooling schedule >= 5kbp, >= 2kbp,\n'+\
-                '\t\t\t    >= 1kbp\n'+\
+                '\t\t\t  format: [1,2,5]\n'+\
+                '\t\t\t  [1,2,5] implies the cooling schedule > 5kbp, 2kbp, 1kbp\n'+\
                 '-p, --path\t\tPath to RAIphy'
                 
-defaultSchedule = [100,50,20,10,5,2,1]
+defaultSchedule = [1,2,5,10,20,50,100]
