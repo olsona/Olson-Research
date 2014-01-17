ff06b8e16a4e586159da3565e79aa440360dfb69
Anna Olson <olsona@cs.uchicago.edu>
2014-01-17 11:43:00 -0600

    Test test test

--- bootstrap_new.py
+++ bootstrap_new.py
@@ -95,7 +95,7 @@ def main(argv):
     fNext = newName
     genePath = newName.rsplit("/",1)[0]+"/contigs/"
     os.system("mkdir {!s}".format(genePath))
-    for i in range(len(coolingSchedule)):]
+    for i in range(len(coolingSchedule)):
         #for i in [0]:
         workingFile = fNext
         thr = int(coolingSchedule[i])
