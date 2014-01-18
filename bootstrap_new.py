e35973e2ae876115c2ae59e8ed68e5a1ca6de0a8
Anna Olson <olsona@cs.uchicago.edu>
2014-01-17 22:06:08 -0600

    dumb dumb dumb

--- bootstrap_new.py
+++ bootstrap_new.py
@@ -116,25 +116,25 @@ def main(argv):
     matches = baseName + "_matches"
     for i in range(len(coolingSchedule)-1,-1,-1):
         # make DB out of fSeed, whatever it is right now
-        os.system("{!s}rait -new -i {!s}-2 -o {!s}-{!s}".format(raiPath, fSeed, DB, i))
+        os.system("{!s}rait -new -i {!s}-2 -o {!s}-{!s} >/dev/null 2>&1".format(raiPath, fSeed, DB, i))
         # match ith contigs to DB
         toMatch = "{!s}_{!s}".format(baseName,i)
-        #os.system("{!s}rai -I {!s}-1 -d {!s} -o {!s}".format(raiPath, toMatch, DB, matches))
+        os.system("{!s}rai -I {!s}-1 -d {!s} -o {!s}".format(raiPath, toMatch, DB, matches))
         # construct matching dictionary
-        #mDict = {}
-        #fMatch = open(matches,'r')
-        #for l in fMatch.readlines():
-        #    [u1,u2] = l.rstrip().split(" ")
-        #    if u2 in matches:
-        #        mDict[u2].append(u1)
-        #    else:
-        #        mDict[u2] = [u1]
+        mDict = {}
+        fMatch = open(matches,'r')
+        for l in fMatch.readlines():
+            [u1,u2] = l.rstrip().split(" ")
+            if u2 in matches:
+                mDict[u2].append(u1)
+            else:
+                mDict[u2] = [u1]
         
         fSeed = toMatch
 
-    #fOut = open(outputFile, 'w')
-    #fOut.write(pprint(mDict))
-    #fOut.close()
+    fOut = open(outputFile, 'w')
+    fOut.write(pprint(mDict))
+    fOut.close()
 
 
     # process results from main loop to get initial "trees"
