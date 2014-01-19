cb9e6a45862502d787e4b6cb07b3e88868045b09
Anna Olson <olsona@cs.uchicago.edu>
2014-01-18 21:07:13 -0600

    dumb dumb dumb

--- bootstrap_new.py
+++ bootstrap_new.py
@@ -123,9 +123,9 @@ def main(argv):
         # match ith contigs to DB
         toMatch = "{!s}_{!s}".format(baseName,i)
         os.system("{!s}rai -I {!s}-1 -d {!s}-{!s}".format(raiPath, toMatch, DB, i))
-        #short = toMatch.rsplit("/",1)[1]
-        #os.system("cp {!s}/{!s}-1.bin {!s}".format(os.getcwd(), short, matches)) # moves results to results folder
-        #os.system("rm {!s}/{!s}-1.bin".format(os.getcwd(), short))
+        short = toMatch.rsplit("/",1)[1]
+        os.system("cp {!s}/{!s}-1.bin {!s}".format(os.getcwd(), short, matches)) # moves results to results folder
+        os.system("rm {!s}/{!s}-1.bin".format(os.getcwd(), short))
         # construct matching dictionary
         #mDict = {}
         #fMatch = open(matches,'r')
