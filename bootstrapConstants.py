d9d99687ce134aed8236447b635d84b82648c24b
Anna Olson <olsona@cs.uchicago.edu>
2014-01-21 16:03:49 -0600

    concatenate, sans printing

--- bootstrapConstants.py
+++ bootstrapConstants.py
@@ -5,9 +5,9 @@ usageString = 'bootstrap.py -i <input file> -o <output file> -p <RAIphy path> '\
                 
 bigUsageString = '-i, --ifile\t\tInput metagenomics FASTA file\n'+\
                 '-o, --ofile\t\tOutput file\n'+\
-                '-c, --schedule\tCooling schedule (optional)\n'+\
+                '-s, --schedule\tpartition schedule (optional)\n'+\
                 '\t\t\t  format: [1,2,5]\n'+\
-                '\t\t\t  [1,2,5] implies the cooling schedule > 5kbp, 2kbp, 1kbp\n'+\
+                '\t\t\t  [1,2,5] implies the partition schedule > 5kbp, 2kbp, 1kbp\n'+\
                 '-p, --path\t\tPath to RAIphy'
                 
 defaultSchedule = [1,2,5,10,20,50,100]
