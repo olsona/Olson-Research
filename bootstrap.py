014b934c1832652ca8a1f6baf9cc76bd51098d56
Anna Olson <olsona@cs.uchicago.edu>
2014-01-21 13:49:38 -0600

    concatenate, sans printing

--- bootstrap.py
+++ bootstrap.py
@@ -121,8 +121,8 @@ def main(argv):
     # Main loop: iterate through cooling schedule, creating databases, making matches, and once matches are made, concatenate each seed (pseudo)contig with matched contigs to make next round
     DB = baseName + "_DB"
     matches = baseName + "_matches"
-    #for i in range(leng-1,-1,-1):
-    for i in [leng-1]:
+    for i in range(leng-1,-1,-1):
+    #for i in [leng-1]:
         # Make DB out of fSeed, whatever it is right now
         os.system("{!s}rait -new -i {!s}-2 -o {!s}-{!s} >/dev/null 2>&1".format(raiPath, fSeed, DB, i))
         # Match ith contigs to DB
