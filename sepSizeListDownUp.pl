172905a651a01ab7855be01304f9972973cc181b
Anna Olson <olsona@cs.uchicago.edu>
2014-01-24 12:52:40 -0600

    Fix problem that shows up in Iowa set

--- sepSizeListDownUp.pl
+++ sepSizeListDownUp.pl
@@ -7,14 +7,14 @@ open(BGGR, '>', $bigger);
 while (<IN>) {
     chomp;
     ($name, $seq) = split("\t");
-    $len = length($seq)
+    $len = length($seq);
     if ( $len >= $threshold ) {
         print BGGR "$name\t$seq\n";
     } else {
         $nname = substr $name, 1;
         $allname = $nname."$len";
         open(FI, '>', "$path$allname.fna");
-        print FI "$allname\n$seq";
+        print FI "$name\n$seq";
         close(FI);
         print SMLR1 "$path$allname.fna\n";
         print SMLR2 "$allname\t$path$allname.fna\n";
