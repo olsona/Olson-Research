2a8a081fae1991c5a9446c4b095a1906bc7f2942
Anna Olson <olsona@cs.uchicago.edu>
2014-02-20 14:14:34 -0600

    Debugging

--- bootstrap.py
+++ bootstrap.py
@@ -205,7 +205,7 @@ def main(argv):
     # process results from main loop to get clusters and distances
     fOutC = open("{!s}_clusters".format(outputFile),'w')
     for c in allClusters:
-        fOutC.write("{!s}\n".format(allClusters[c].get_leaves())
+        fOutC.write("{!s}\n".format(allClusters[c].get_leaves()))
     fOutC.close()
 
     # get distances between extant clusters
