2ca25107733e96d7e98a0ef8f475b6704db5c11a
Anna Olson <olsona@cs.uchicago.edu>
2014-02-27 00:08:53 -0600

    Duh

--- kikiContig2FASTA.pl
+++ kikiContig2FASTA.pl
@@ -1,9 +1,15 @@
 #!/usr/bin/perl
 my $input = $ARGV[0];
-my $prename = substr($input, 0, -7);
+my $output = $ARGV[1];
+my $name = substr($input, 0, -7);
+my $ct = 0;
 open(IN, $input);
+open(OUT, $output);
 while (<IN>) {
     chomp;
-    ($a,$b,$c,$d,$e,$f,$g) = split("\t");
-    printf $f . "\n";
+    ($a,$sz,$c,$d,$e,$f,$g) = split("\t");
+    print OUT "$name_$sz_$ct\n$g\n";
+    ct++;
 }
+close(IN);
+close(OUT);
