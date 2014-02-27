f14d9ac78b25881b6954f357bcc22b4a03b6a4d3
Anna Olson <olsona@cs.uchicago.edu>
2014-02-26 23:56:34 -0600

    Duh

--- kikiContig2FASTA.pl
+++ kikiContig2FASTA.pl
@@ -1,5 +1,5 @@
 #!/usr/bin/perl
 my $input = $ARGV[0];
 my $prename = substr($input, 0, -7);
-my $name = $(split(/\\/, $prename))[-1];
-printf $name . "/n";
+my @fname = split(/\\/, $prename);
+printf $fname . "/n";
