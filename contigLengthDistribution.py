9583eaceb8e8f6ae30beac2164508629824b0f09
Anna Olson <olsona@cs.uchicago.edu>
2014-02-27 10:36:33 -0600

    debugging again

--- contigLengthDistribution.py
+++ contigLengthDistribution.py
@@ -7,6 +7,6 @@ def dist(infile):
     f.close()
     lens = [0.0 for i in range(num/2)]
     for i in range(num/2):
-        lens[i] = len(lines[i*2]+1)
+        lens[i] = len(lines[(i*2)+1])
     pyplot.hist(lens)
     pyplot.show()
