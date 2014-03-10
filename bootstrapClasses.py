4d3d1fd39a85fe56f491aecaa3e7ac6ce450052c
Anna Olson <olsona@cs.uchicago.edu>
2014-03-10 14:10:57 -0500

    debugging forever

--- bootstrapClasses.py
+++ bootstrapClasses.py
@@ -1,62 +1,70 @@
 from bootstrapUtils import *
 
 class Cluster:
-    def __init__(self, seed, dict = None, root = None, closeList = None):
-        self.seed = seed
-        if dict is None:
-            self.dict = {}
-        else:
-            self.dict = dict
-        if root is None:
-            self.root = self.seed
-        else:
-            self.root = root
-        if closeList is None:
-            self.closeList = {}
-        else:
-            self.closeList = closeList
+	def __init__(self, seed, dict = None, root = None, closeList = None):
+		self.seed = seed
+		if dict is None:
+			self.dict = {}
+		else:
+			self.dict = dict
+		if root is None:
+			self.root = self.seed
+		else:
+			self.root = root
+		if closeList is None:
+			self.closeList = {}
+		else:
+			self.closeList = closeList
 
-    def __str__(self):
-        return "Seed: {!s}\tLeaves: {!s}".format(self.seed,self.get_leaves())
+	def __str__(self):
+		return "Seed: {!s}\tLeaves: {!s}".format(self.seed,self.get_leaves())
 
-    def get_leaves(self):
-        if self.root is None or self.dict is None:
-            return []
-        else:
-            return getLeaves(self.dict, self.root)
+	def getLeaves(self):
+		if self.root is None or self.dict is None:
+			return []
+		else:
+			return getLeaves(self.dict, self.root)
 
-    def purityMax(self, names):
-        if self.dict is None:
-            return None, None
-        else:
-            return purityOfCluster(dict, names)
+	def purityMax(self, names):
+		if self.dict is None:
+			return None, None
+		else:
+			return purityOfCluster(dict, names)
 
-    def addNode(self, parent, child):
-        if parent in self.dict:
-            self.dict[parent].append(child)
-        else:
-            self.dict[parent] = [child]
+	def addNode(self, parent, child):
+		if parent in self.dict:
+			self.dict[parent].append(child)
+		else:
+			self.dict[parent] = [child]
 
-    def get_All(self):
-        if self.root is None or self.dict is None:
-            return []
-        else:
-            return self.get_leaves().append(self.root)
+	def getAll(self):
+		if self.root is None or self.dict is None:
+			return []
+		else:
+			return self.getLeaves().append(self.root)
+
+	def addMatch(self, goodMatch):
+		name = goodMatch[0]
+		score = goodMatch[1]
+		if name in self.closeList:
+			self.closeList[name].append(score)
+		else:
+			self.closeList[name] = [score]
 
 # THE TREES ARE UPSIDE DOWN!  dict[j] is all of the *parents* of j!
 
 
 class Contig:
-    def __init__(self, name, myCluster=None, good_matches=None):
-        self.name = name
-        if myCluster is None:
-            self.myCluster = None
-        else:
-            self.myCluster = myCluster
-        if good_matches is None:
-            self.good_matches = []
-        else:
-            self.good_matches = good_matches
+	def __init__(self, name, myCluster=None, goodMatches=None):
+		self.name = name
+		if myCluster is None:
+			self.myCluster = None
+		else:
+			self.myCluster = myCluster
+		if goodMatches is None:
+			self.goodMatches = []
+		else:
+			self.goodMatches = goodMatches
 
-    def __str__(self):
-        return "Name: {!s}\nFile: {!s}\nCluster: {!s}".format(self.name, self.myCluster)
+	def __str__(self):
+		return "Name: {!s}\nFile: {!s}\nCluster: {!s}".format(self.name, self.myCluster)
--- bootstrapConstants.py
+++ bootstrapConstants.py
@@ -1,15 +1,15 @@
 myMethods = ['raiphy']
 
 usageString = 'bootstrap.py -i <input file> -o <output file> -p <RAIphy path> '\
-            '[-r <reference database> -c <cooling schedule>]'
-                
+			'[-r <reference database> -c <cooling schedule>]'
+				
 bigUsageString = '-i, --ifile\t\tInput metagenomics FASTA file\n'+\
-                '-o, --ofile\t\tOutput file\n'+\
-                '-s, --schedule\tpartition schedule (optional)\n'+\
-                '\t\t\t  format: [1,2,5]\n'+\
-                '\t\t\t  [1,2,5] implies the partition schedule > 5kbp, 2kbp, 1kbp\n'+\
-                '-p, --path\t\tPath to RAIphy'
-                
+				'-o, --ofile\t\tOutput file\n'+\
+				'-s, --schedule\tpartition schedule (optional)\n'+\
+				'\t\t\t	 format: [1,2,5]\n'+\
+				'\t\t\t	 [1,2,5] implies the partition schedule > 5kbp, 2kbp, 1kbp\n'+\
+				'-p, --path\t\tPath to RAIphy'
+				
 defaultSchedule = [1,2,5,10,20,50,100]
 
 matchThreshold = -0.75
