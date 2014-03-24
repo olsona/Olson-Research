423b2e16a5883448ecf3b5eff737f00e3190b881
Anna Olson <olsona@cs.uchicago.edu>
2014-03-24 17:08:31 -0500

    Debugging forever

--- bootstrapConstants.py
+++ bootstrapConstants.py
@@ -3,15 +3,15 @@ from bootstrapUtils import *
 myMethods = ['raiphy']
 
 usageString = 'bootstrap.py -i <input file> -o <output file> -p <RAIphy path> '\
-            '[-r <reference database> -c <cooling schedule>]'
-                
+			'[-r <reference database> -c <cooling schedule>]'
+				
 bigUsageString = '-i, --ifile\t\tInput metagenomics FASTA file\n'+\
-                '-o, --ofile\t\tOutput file\n'+\
-                '-s, --schedule\tpartition schedule (optional)\n'+\
-                '\t\t\t  format: [1,2,5]\n'+\
-                '\t\t\t  [1,2,5] implies the partition schedule > 5kbp, 2kbp, 1kbp\n'+\
-                '-p, --path\t\tPath to RAIphy'
-                
+				'-o, --ofile\t\tOutput file\n'+\
+				'-s, --schedule\tpartition schedule (optional)\n'+\
+				'\t\t\t	 format: [1,2,5]\n'+\
+				'\t\t\t	 [1,2,5] implies the partition schedule > 5kbp, 2kbp, 1kbp\n'+\
+				'-p, --path\t\tPath to RAIphy'
+				
 defaultSchedule = [1,2,5,10,20,50,100]
 
 matchThreshold = -0.5
@@ -20,10 +20,10 @@ closeThreshold = 0.1
 
 #http://my.safaribooksonline.com/book/programming/python/0596007973/python-shortcuts/pythoncook2-chp-4-sect-16
 correctnessDict = {
-    "species":  checkCorrectSpeciesOlsonFormat,
-    "genus":    checkCorrectGenusOlsonFormat,
+	"species":	checkCorrectSpeciesOlsonFormat,
+	"genus":	checkCorrectGenusOlsonFormat,
 }
 
 scoringMethodDict = {
-    "raiphy":   scoreRAIphy,
+	"raiphy":	scoreRAIphy,
 }
