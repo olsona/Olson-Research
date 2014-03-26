37ccb5effd3bee27cef9388571dd7cd6d7063214
Anna Olson <olsona@cs.uchicago.edu>
2014-03-26 00:44:59 -0500

    Debugging forever

--- tetra.pl
+++ tetra.pl
@@ -4,13 +4,14 @@ use strict;
 use warnings;
 use Statistics::Basic qw(:all nofill);
 
-my ($DBfile) = @ARGV;
+my ($DBfile,$matchfile,$outfile) = @ARGV;
+
+open (OUT, '>', $outfile);
 
 my @DBlines = `cat $DBfile`;
 my @DBvectors = ();
 my @DBnames = ();
 foreach my $dbl (@DBlines) {
-    #print $dbl;
     my @dbln = split ':', $dbl;
     push(@DBnames, $dbln[0]);
     shift @dbln;
@@ -18,4 +19,30 @@ foreach my $dbl (@DBlines) {
     push(@DBvectors, $dbv);
 }
 
-print "@DBnames\n";
+my @matchlines = `cat $matchfile`;
+my @matchnames = ();
+my @matchvectors = ();
+foreach my $mal (@matchlines) {
+    my @maln = split ':', $mal;
+    push(@matchnames, $maln[0]);
+    shift @maln;
+    my $mav = vector(@maln);
+    push(@matchvectors, $mav);
+}
+
+my $dbstr = join(',',@DBnames);
+print OUT "$dbstr\n";
+my $mstr = join(',',@matchnames);
+print OUT "$mstr\n";
+
+foreach my $mv (@matchvectors) {
+    my @zres = ();
+    foreach my $dv (@DBvectors) {
+        my $cor = corr($mv, $dv);
+        push(@zres,$cor);
+    }
+    my $zstr = join(',',@zres);
+    print OUT "$zstr\n";
+}
+
+close(OUT);
