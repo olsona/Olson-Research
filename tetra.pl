8db7af51479c58b821889731a0dea25197b9b37d
Anna Olson <olsona@cs.uchicago.edu>
2014-03-26 00:13:51 -0500

    Debugging forever

--- tetra.pl
+++ tetra.pl
@@ -2,7 +2,7 @@
 
 use strict;
 use warnings;
-use Statistics::Basic qw(:all nofill);
+#use Statistics::Basic qw(:all nofill);
 
 my ($DBfile) = @ARGV;
 
@@ -14,5 +14,5 @@ foreach my $dbl (@DBlines) {
     #print $dbl;
     my @dbln = split ':', $dbl;
     shift @dbln;
-    print @dbln[0];
+    my $dbv = vector(@dbln);
 }
