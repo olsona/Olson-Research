1fae7f90b77e95a93963cb249737e771026c8680
Anna Olson <olsona@cs.uchicago.edu>
2014-03-28 20:31:44 -0500

    Now it's time for TACOA

---
+++ tacoaCount.pl
@@ -0,0 +1,26 @@
+#!/usr/bin/perl
+
+use strict;
+use warnings;
+
+my ($infile, $k, $tcount, $countfile) = @ARGV;
+
+open (IN, $infile);
+
+my $c = 0;
+my $g = 0;
+my $total = 0;
+while (<IN>) {
+    chomp;
+    my $_ = lc($_);
+    $total += length;
+    $c += ($_ =~ tr/c//);
+    $g += ($_ =~ tr/g//);
+}
+
+close(IN);
+my $gc = $g+$c;
+
+my $scount = 4 ** $k;
+system("jellyfish count -C -m ${k} -s ${scount} -t ${tcount} -o ${countfile}.jf ${infile}");
+system("jellyfish dump -c ${countfile}.jf > ${countfile}");
