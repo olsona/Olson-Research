3e2cb3ab3b2b757d3be513c6e17155297502dc11
Anna Olson <olsona@cs.uchicago.edu>
2014-04-02 22:35:32 -0500

    debugging forever

--- jellyfishZscores.pl
+++ jellyfishZscores.pl
@@ -3,6 +3,30 @@
 use strict;
 use warnings;
 
+
+# Generate a hashref of each kmer possibility
+sub kmer_generator {
+	my $k = shift;
+	my $kmer_p;
+    
+	my @bases = ('A','C','G','T');
+	my @words = @bases;
+    
+	for (my $i=1;$i<$k;$i++) {
+		my @newwords;
+		foreach my $w (@words) {
+			foreach my $b (@bases) {
+				push (@newwords,$w.$b);
+			}
+		}
+		undef @words;
+		@words = @newwords;
+	}
+	map{ $kmer_p->{$_}=0} @words;
+	return($kmer_p);
+}
+
+
 # Read given index file and return a hashref of names and filenames
 sub readIndex {
 	my ($indexFile) = @_;
@@ -23,6 +47,7 @@ sub readIndex {
 	return $inputFiles;
 }
 
+# Reads counts from jellyfish count files
 sub getCounts {
     my ($inFile) = @_;
     my $kmrHash;
@@ -38,10 +63,18 @@ sub getCounts {
 #---MAIN CODE---#
 my ($jellyfishPath, $indexFile, $resultsFile) = @ARGV;
 
+# Read index file
 my $inputFiles = readIndex($indexFile);
 
+# Open results file
+open(my $resultsfh, '>', $resultsFile) or die "FATAL: Unable to write results file: $!\n";
+
+# Main loop
 foreach my $inputName (sort keys %$inputFiles) {
     my $inputFile = $inputFiles->{$inputName};
+    print STDOUT "Processing File: $inputName - $inputFile\n";
+    
+    # prepare for running Jellyfish
     my $filesize = -s $inputFile;
     my $hsh = int($filesize/4);
     my $jf4 = "${inputFile}_JF_4";
@@ -51,6 +84,7 @@ foreach my $inputName (sort keys %$inputFiles) {
     my $ct3 = "${inputFile}_ct_3";
     my $ct2 = "${inputFile}_ct_2";
     
+    # Run Jellyfish
     system("${jellyfishPath}/jellyfish count -m 4 -s ${hsh} -t 10 -o ${jf4} -C ${inputFile}");
     system("${jellyfishPath}/jellyfish dump -c ${jf4} > ${ct4}");
     system("rm ${jf4}");
@@ -61,11 +95,40 @@ foreach my $inputName (sort keys %$inputFiles) {
     system("${jellyfishPath}/jellyfish dump -c ${jf2} > ${ct2}");
     system("rm ${jf2}");
     
+    # Get results from Jellyfish
     my $kmr4 = getCounts($ct4);
     my $kmr3 = getCounts($ct3);
     my $kmr2 = getCounts($ct2);
     
-    foreach my $w1h (sort (keys %$kmr4)) {
-        print $w1h."\n";
+    my @all4mers = sort keys kmer_generator(4);
+    
+    # Generate and write results
+    print $resultsfh $inputName;
+    foreach my $w1h (@all4mers) {
+        my $w1h1	= substr($w1h, 0, $h_1);
+		my $w2h1	= substr($w1h, 1, $h_1-1);
+		my $w2h		= substr($w1h, 1, $h_1);
+        
+        my $Nw		= $kmr4->{$w1h};
+		my $Nw1h1	= $kmr3->{$w1h1};
+		my $Nw2h	= $kmr3->{$w2h};
+		my $Nw2h1	= $kmr2->{$w2h1};
+        
+        my $Nhathat = 0;
+        my $Vhathat = 0;
+        if ($Nw2h1 >= 1) {
+            $Nhathat = ($Nw1h1*$Nw2h)/$Nw2h1;
+            $Vhathat = (($Nw1h1*$Nw2h)/($Nw2h1*$Nw2h1*$Nw2h1)) * ($Nw2h1-$Nw1h1) * ($Nw2h1-$Nw2h);
+        }
+        
+        my $ZM      = 0;
+        if ($Nhathat > 0 && $Vhathat > 0) {
+            $ZM     = ($Nw-$Nhathat)/sqrt($Vhathat);
+        }
+        
+        printf $resultsfh ":%.8f", $ZM;
     }
+    print $resultsfh "\n";
 }
+
+close($resultsfh);
