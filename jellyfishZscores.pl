b872a3159d1808036cab9742f936d98d0151c1c7
Anna Olson <olsona@cs.uchicago.edu>
2014-04-02 22:17:51 -0500

    debugging forever

--- jellyfishZscores.pl
+++ jellyfishZscores.pl
@@ -45,24 +45,23 @@ foreach my $inputName (sort keys %$inputFiles) {
     my $filesize = -s $inputFile;
     my $hsh = int($filesize/4);
     my $jf4 = "${inputFile}_JF_4";
-    #my $jf3 = "${inputFile}_JF_3";
-    #my $jf2 = "${inputFile}_JF_2";
+    my $jf3 = "${inputFile}_JF_3";
+    my $jf2 = "${inputFile}_JF_2";
     my $ct4 = "${inputFile}_ct_4";
-    #my $ct3 = "${inputFile}_ct_3";
-    #my $ct2 = "${inputFile}_ct_2";
+    my $ct3 = "${inputFile}_ct_3";
+    my $ct2 = "${inputFile}_ct_2";
     
     system("${jellyfishPath}/jellyfish count -m 4 -s ${hsh} -t 10 -o ${jf4} -C ${inputFile}");
     system("${jellyfishPath}/jellyfish dump -c ${jf4} > ${ct4}");
     system("rm ${jf4}");
-    #system("${jellyfishPath}/jellyfish count -m 3 -s ${hsh} -t 10 -o ${jf3} -C ${inputFile}");
-    #system("${jellyfishPath}/jellyfish dump -c ${jf3} > ${ct3}");
-    #system("rm ${jf3}");
-    #system("${jellyfishPath}/jellyfish count -m 2 -s ${hsh} -t 10 -o ${jf2} -C ${inputFile}");
-    #system("${jellyfishPath}/jellyfish dump -c ${jf2} > ${ct2}");
-    #system("rm ${jf2}");
+    system("${jellyfishPath}/jellyfish count -m 3 -s ${hsh} -t 10 -o ${jf3} -C ${inputFile}");
+    system("${jellyfishPath}/jellyfish dump -c ${jf3} > ${ct3}");
+    system("rm ${jf3}");
+    system("${jellyfishPath}/jellyfish count -m 2 -s ${hsh} -t 10 -o ${jf2} -C ${inputFile}");
+    system("${jellyfishPath}/jellyfish dump -c ${jf2} > ${ct2}");
+    system("rm ${jf2}");
     
     my $kmr4 = getCounts($ct4);
-    foreach (keys %$kmr4){
-        print $_.", ".$kmr4->{$_}."\n";
-    }
+    my $kmr3 = getCounts($ct3);
+    my $kmr2 = getCounts($ct2);
 }
