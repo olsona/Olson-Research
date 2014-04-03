3b0ac58984e851e458e94d3075f84bf26aafc0f3
Anna Olson <olsona@cs.uchicago.edu>
2014-04-02 21:51:06 -0500

    debugging forever

--- jellyfishZscores.pl
+++ jellyfishZscores.pl
@@ -34,6 +34,6 @@ foreach my $inputName (sort keys %$inputFiles) {
     my $filesize = -s $inputFile;
     my $hsh4 = int($filesize/4);
     my $work4 = "${workingPrefix}_${inputFile}_4";
-    echo $work4;
-    system("$jellyfishPath/jellyfish count -m 4 -s $hsh4 -t 10 -o $work4 -C $inputFile");
+    print $work4;
+    system("${jellyfishPath}/jellyfish count -m 4 -s ${hsh4} -t 10 -o ${work4} -C ${inputFile}");
 }
