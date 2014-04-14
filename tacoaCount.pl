bfafbd16048ccb88f96e9d52e151406747aed8a3
Anna Olson <olsona@cs.uchicago.edu>
2014-04-13 22:47:23 -0500

    Debugging forever

--- tacoaCount.pl
+++ tacoaCount.pl
@@ -207,7 +207,7 @@ foreach my $inputName (sort keys %$inputFiles) {
             my @strarray = unpack 'C*', $o;
             my $pr = 1.0;
             foreach my $c (@strarray) {
-                printf ":%.8f\n", $pr;
+                printf ":%.8f\n", $pr * $probs{$c};
                 $pr *= $probs{$c};
             }
             my $Eo = $pr*$s;
