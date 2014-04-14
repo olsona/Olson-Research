dc7b60feeb51c5634c96b68b939702b733dc29c1
Anna Olson <olsona@cs.uchicago.edu>
2014-04-13 22:29:38 -0500

    Debugging forever

--- tacoaCount.pl
+++ tacoaCount.pl
@@ -205,9 +205,9 @@ foreach my $inputName (sort keys %$inputFiles) {
         my $Oo = $kCounts->{$o};
         if ($Oo != 0) {
             my @strarray = unpack 'C*', $o;
-            my $pr = 1;
+            my $pr = 1.0;
             foreach my $c (@strarray) {
-                $pr *= $probs{$c}
+                $pr *= $probs{$c};
             }
             my $Eo = $pr*$s;
             if ($Oo > $Eo) {
