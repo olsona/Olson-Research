caecd7d4dee54121279951a1e4ad6126901b4da2
Anna Olson <olsona@cs.uchicago.edu>
2014-05-07 19:13:57 -0500

    debugging forever

--- bootstrap.py
+++ bootstrap.py
@@ -328,6 +328,7 @@ def main(argv):
 			l2.write("{!s}\t{!s}{!s}.fna\n".format(newContig,genePath,newContig))
 		
 		for item in toPop:
+			print "Popped {!s}".format(item)
 			allClusters.pop(item)
 					
 		# *** compute correctness distributions
