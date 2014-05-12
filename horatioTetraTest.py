fbb160e3c3cd6a5e2ed19eef875d49d2048c3124
Anna Olson <olsona@cs.uchicago.edu>
2014-05-12 14:33:25 -0500

    debugging forever

--- horatioTetraTest.py
+++ horatioTetraTest.py
@@ -33,8 +33,8 @@ def main(argv):
                 for s in sList:
                     splitList = [s]*(len(t)-1)
                     #myOut = "{!s}_N_{!s}_J_{!s}_C_{!s}_L_{!s}".format(out,n,j,t,splitList)
-                    tStr = "["+",".join(t)+"]"
-                    sStr = "["+",".join(splitList)+"]"
+                    tStr = "["+",".join([str(ti) for ti in t])+"]"
+                    sStr = "["+",".join([str(si) for si in splitList])+"]"
                     print "python horatio.py -i {!s} -o {!s} -s tetra -n {!s} -j {!s} -c {!s} -l {!s}".format(myfile,\
                         out, n, j, tStr, sStr)
                     os.system("time python horatio.py -i {!s} -o {!s} -s tetra -n {!s} -j {!s} -c {!s} -l {!s}".format(myfile,\
