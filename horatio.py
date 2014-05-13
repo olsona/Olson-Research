0c2e77c5b15af8e91df7d74ace46ff406700608d
Anna Olson <olsona@cs.uchicago.edu>
2014-05-12 22:33:29 -0500

    debugging forever

--- horatio.py
+++ horatio.py
@@ -345,9 +345,9 @@ def main(argv):
 	rdata = rightDists[iterString]
 	wdata = wrongDists[iterString]
 	if rdata:
-	    print "Right ({!s}): {:03.2f}-{:03.2f}".format(len(rdata),min(rdata),max(rdata))
+	    print "Right ({!s}): {:03.2f} - {:03.2f}".format(len(rdata),min(rdata),max(rdata))
 	if wdata:
-	    print "Wrong ({!s}): {:03.2f}-{:03.2f}".format(len(wdata),min(wdata),max(wdata))
+	    print "Wrong ({!s}): {:03.2f} - {:03.2f}".format(len(wdata),min(wdata),max(wdata))
 	hcorr.comparisonPlot(rdata, wdata, iterString, outputFile, "seed", "Correct distances", "Incorrect Distances")
 	# ***   
         
---
+++ horatioTest.py
@@ -0,0 +1,49 @@
+#! /usr/bin/env python
+
+import sys, os, getopt
+
+def main(argv):
+    myfile = ''
+    out = ''
+    try:
+	opts, args = getopt.getopt(argv,"f:o:")
+    except getopt.GetoptError:
+	sys.exit(2)
+        
+    for opt, arg in opts:
+        if opt == "-f":	
+            myfile = arg
+        elif opt == "-o":
+            out = arg	
+    
+    outlog = out + "_log"
+    fOut = open(outlog,'w')
+    
+    jList = [0.1,0.2,0.3,0.4,0.5,0.6,0.7,0.8,0.9]
+    nList = [0.01,0.02,0.05,0.1,0.2]
+    tDict = {'all_close': [2,4,6,8,10,12,14,16,18,20,22],
+            'low_close': [2,4,6,8,10,14,18,22],
+            '2by4': [2,6,10,14,18,22],
+            '4by4': [4,8,12,16,20]}
+    sDict = {'tacoa': [0.3,0.35,0.4,0.45,0.5],
+            'tetra': [0.3,0.4,0.5,0.6,0.7,0.8],
+            'raiphy': []}
+    for score in sDict:
+        sList = sDict[score]
+        for t in tDict:
+            for s in sList:
+                for j in jList:
+                    for n in nList:
+                        splitList = [s]*(len(tDict[t])-1)
+                        tStr = "["+",".join([str(ti) for ti in tDict[t]])+"]"
+                        sStr = "["+",".join([str(si) for si in splitList])+"]"
+                        myOut = "{!s}_{!s}_N_{!s}_J_{!s}_C_{!s}_L_{!s}".format(out,score,n,j,t,s)
+                        print "python horatio.py -i {!s} -o {!s} -s {!s} -n {!s} -j {!s} -c {!s} -l {!s}".\
+                            format(myfile,myOut, score, n, j, tStr, sStr)
+                        os.system("python horatio.py -i {!s} -o {!s} -s {!s} -n {!s} -j {!s} -c {!s} -l {!s}".\
+                            format(myfile, myOut, score, n, j, tStr, sStr))
+
+    fOut.close()            
+
+if __name__ == "__main__":
+    main(sys.argv[1:])
