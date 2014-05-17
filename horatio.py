f0e4995fc6fb1bc99e581ace7ddab703470a7467
Anna Olson <olsona@cs.uchicago.edu>
2014-05-17 15:55:18 -0500

    Better testing

--- horatio.py
+++ horatio.py
@@ -371,12 +371,12 @@ def main(argv):
     pickle.dump(totalCluster,open("{!s}_clusters_pickle".format(outputFile),"wb"))
     
     # Get rid of files we're not using any more
-    os.system("rm -r {!s}".format(genePath))
-    os.system("rm {!s}".format(DB))
-    os.system("rm {!s}".format(toMatch))
-    os.system("rm {!s}".format(fSeed))
+    os.system("rm -r {!s} >/dev/null".format(genePath))
+    os.system("rm {!s} >/dev/null".format(DB))
+    os.system("rm {!s} >/dev/null".format(toMatch))
+    os.system("rm {!s} >/dev/null".format(fSeed))
     for i in range(leng+1):
-	os.system("rm {!s}_{!s}*".format(baseName,i))
+	os.system("rm {!s}_{!s}* >/dev/null".format(baseName,i))
     
 
 if __name__ == "__main__":
