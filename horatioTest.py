ebc1739073c754bfffae650b09d6e7c351ad4ae8
Anna Olson <olsona@cs.uchicago.edu>
2014-05-17 17:06:42 -0500

    debugging

--- horatioTest.py
+++ horatioTest.py
@@ -6,7 +6,7 @@ def main(argv):
     myfile = ''
     out = ''
     try:
-	opts, args = getopt.getopt(argv,"f:o:p:")
+	opts, args = getopt.getopt(argv,"f:o:p:s")
     except getopt.GetoptError:
 	sys.exit(2)
         
@@ -17,6 +17,8 @@ def main(argv):
             out = arg	
         elif opt == "-p":
             path = arg
+        elif opt == "-s":
+            score = arg
     
     outlog = out + "_log"
     fOut = open(outlog,'w')
@@ -30,28 +32,28 @@ def main(argv):
     sDict = {'tacoa': [0.3,0.4,0.5],
             'tetra': [0.2,0.4,0.6,0.8],
             'raiphy': [-17.0,-16.0,-15.0,-14.0]}
-    for score in sorted(sDict.keys()):
-        sList = sDict[score]
-        for t in tDict:
-            for s in sList:
-                for j in jList:
-                    for n in nList:
-                        splitList = [s]*(len(tDict[t])-2)
-                        splitList.append(-20.0)
-                        tStr = "["+",".join([str(ti) for ti in tDict[t]])+"]"
-                        sStr = "["+",".join([str(si) for si in splitList])+"]"
-                        print "{!s}, {!s}, {!s}, {!s}, {!s}". format(score, n, j, tStr, sStr)
-                        myOut = "{!s}_{!s}_N_{!s}_J_{!s}_C_{!s}_L_{!s}".format(out,score,n,j,t,s)
-                        fOut.write("{!s},{!s},{!s},{!s},{!s},".\
-                            format(score, n, j, tStr, sStr))
-                        start = time.time()
-                        os.system("python horatio.py -i {!s} -o {!s} -s {!s} -n {!s} -j {!s} -c {!s} -l {!s} -p {!s}".\
-                            format(myfile, myOut, score, n, j, tStr, sStr, path))
-                        end = time.time()
-                        fOut.write("{:03.2f}\n".format(end-start))
-                        minutes = int((end-start)/60.0)
-                        secs = int((end-start)-60.0*minutes)
-                        print "{!s}:{!s}\n".format(minutes,secs)
+    #for score in sorted(sDict.keys()):
+    sList = sDict[score]
+    for t in tDict:
+        for s in sList:
+            for j in jList:
+                for n in nList:
+                    splitList = [s]*(len(tDict[t])-2)
+                    splitList.append(-20.0)
+                    tStr = "["+",".join([str(ti) for ti in tDict[t]])+"]"
+                    sStr = "["+",".join([str(si) for si in splitList])+"]"
+                    print "{!s}, {!s}, {!s}, {!s}, {!s}". format(score, n, j, tStr, sStr)
+                    myOut = "{!s}_{!s}_N_{!s}_J_{!s}_C_{!s}_L_{!s}".format(out,score,n,j,t,s)
+                    fOut.write("{!s},{!s},{!s},{!s},{!s},".\
+                        format(score, n, j, tStr, sStr))
+                    start = time.time()
+                    os.system("python horatio.py -i {!s} -o {!s} -s {!s} -n {!s} -j {!s} -c {!s} -l {!s} -p {!s}".\
+                        format(myfile, myOut, score, n, j, tStr, sStr, path))
+                    end = time.time()
+                    fOut.write("{:03.2f}\n".format(end-start))
+                    minutes = int((end-start)/60.0)
+                    secs = int((end-start)-60.0*minutes)
+                    print "{!s}:{!s}\n".format(minutes,secs)
 
     fOut.close()            
 
