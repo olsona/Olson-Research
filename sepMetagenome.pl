32d5a3dfb48eb72524d4711305e8b821752627ff
Anna Olson <olsona@cs.uchicago.edu>
2014-05-28 16:15:45 -0500

    for splitting a metagenome

---
+++ sepMetagenome.pl
@@ -0,0 +1,18 @@
+#!/usr/bin/perl
+my ($path, $input, $out) = @ARGV;
+open(IN, $input);
+open(OUT1, '>', "$out-1");
+open(OUT2, '>', "$out-2");
+while (<IN>) {
+    chomp;
+    ($name, $seq) = split("\t");
+    $nname = substr $name, 1;
+    open(FI, '>', "$path$nname.fna");
+    print FI "$name\n$seq";
+    close(FI);
+    print OUT1 "$path$nname.fna\n";
+    print OUT2 "$nname\t$path$nname.fna\n";
+}
+close(IN);
+close(OUT1);
+close(OUT2);
