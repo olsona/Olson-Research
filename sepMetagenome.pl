80a8b07db73aeccfeea9538cdf61226c1da075ac
Anna Olson <olsona@cs.uchicago.edu>
2014-05-28 16:30:56 -0500

    debugging forever

--- sepMetagenome.pl
+++ sepMetagenome.pl
@@ -8,12 +8,11 @@ while (<IN>) {
     ($name, $seq) = split("\t");
     $nname = substr $name, 1;
     $spname = (split '_len', $nname)[0];
-    open(FI, '>', "$path$nname.fna");
-    print FI "$name\n$seq";
-    print $spname."\n";
+    open(FI, '>', "$path$spname.fna");
+    print FI "$>spname\n$seq";
     close(FI);
-    print OUT1 "$path$nname.fna\n";
-    print OUT2 "$nname\t$path$nname.fna\n";
+    print OUT1 "$path$spname.fna\n";
+    print OUT2 "$spname\t$path$spname.fna\n";
 }
 close(IN);
 close(OUT1);
