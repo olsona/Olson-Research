9fde049e2e59ff541d269c2cd14c6ca11627145f
Anna Olson <olsona@cs.uchicago.edu>
2014-05-29 15:08:14 -0500

    siiiiigh

--- renameRefseq.pl
+++ renameRefseq.pl
@@ -9,9 +9,22 @@ for my $input (@files) {
     while (<$IN>) {
         chomp;
         my $cr = substr $_, 0, 1;
-        print $cr;
+        if ($cr == '>' ) {
+            close($outfh) if defined $outfh;
+            my $name = substr $_, 1;
+            my $spname = (split '|', $name)[4];
+            my $cname = (split ',', $spname)[0];
+            open($outfh, '>', "$outpath$cname.fna");
+            print $outfh ">$cname\t";
+            print $OUT1 "$outpath$cname.fna\n";
+            print $OUT2 "$cname\t$outpath$cname.fna\n";
+        }
+        elsif (defined $outfh) {
+            print $outfh $_;
+        }
     }
 }
+close($outfh) if defined $outfh;
 close($IN);
 close($OUT1);
 close($OUT2);
