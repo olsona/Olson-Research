1f5db4912a72e92db65342d26dbea58e7be5dd64
Anna Olson <olsona@cs.uchicago.edu>
2014-05-29 10:09:29 -0500

    siiiiigh

--- renameRefseq.pl
+++ renameRefseq.pl
@@ -1,7 +1,6 @@
 #!/usr/bin/perl
-use File::Slurp;
 my ($inpath, $outpath, $out) = @ARGV;
-my @files = read_dir $inpath;
+my @files = <$inpath/*.fna>;
 open(OUT1, '>', "$out-1");
 open(OUT2, '>', "$out-2");
 for my $input (@files) {
