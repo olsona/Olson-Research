9a83756059acc9579b530e519e10ddae80a765a3
Anna Olson <olsona@cs.uchicago.edu>
2014-05-29 15:03:28 -0500

    siiiiigh

--- renameRefseq.pl
+++ renameRefseq.pl
@@ -5,26 +5,30 @@ open($OUT1, '>', "$out-1");
 open($OUT2, '>', "$out-2");
 for my $input (@files) {
     open($IN, $input);
-    my $FI;
+    my $outfh;
     while (<$IN>) {
         chomp;
-        my $chr = substr $_, 0, 1;
-        if ( $chr == '>' ) {
-            close($FI) if (defined $FI);
-            my $name = substr $_, 1;
-            print $name
-            my $spname = (split '|', $name)[4];
-            my $cname = (split ',', $spname)[0];
-            open($FI, '>', "$outpath$cname.fna");
-            print $FI ">$cname\t";
-            print $OUT1 "$outpath$cname.fna\n";
-            print $OUT2 "$cname\t$outpath$cname.fna\n";
-        }
-        else {
-            print $FI "$_";
-        }
+        my $cr = substr $_, 0, 1;
+        print $cr;
     }
 }
 close($IN);
 close($OUT1);
 close($OUT2);
+
+
+my $chr = substr $_, 0, 1;
+if ( $chr == '>' ) {
+    close($FI) if (defined $FI);
+    my $name = substr $_, 1;
+    print $name
+    my $spname = (split '|', $name)[4];
+    my $cname = (split ',', $spname)[0];
+    open($FI, '>', "$outpath$cname.fna");
+    print $FI ">$cname\t";
+    print $OUT1 "$outpath$cname.fna\n";
+    print $OUT2 "$cname\t$outpath$cname.fna\n";
+}
+else {
+    print $FI "$_";
+}
