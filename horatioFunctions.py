1438ec62acdec2bf9a2336f7f013aea550ac3f9f
Anna Olson <olsona@cs.uchicago.edu>
2014-07-07 22:07:42 -0500

    tab issues

--- horatioFunctions.py
+++ horatioFunctions.py
@@ -74,6 +74,6 @@ def scoreTACOA(DB, fSeed, matches, toMatch, allContigs):
 	
 def scoreTACOAFinal(DB, fSeed, outputFile):
 	print "Step 1"
-    os.system("perl tacoaCount.pl -k 4 {!s} {!s} >/dev/null".format(fSeed,DB))
-    print "Step 2"
+	os.system("perl tacoaCount.pl -k 4 {!s} {!s} >/dev/null".format(fSeed,DB))
+	print "Step 2"
 	os.system("perl tacoaDistance.pl {!s} {!s} {!s}_dists_sorted >/dev/null".format(DB, DB, outputFile))
