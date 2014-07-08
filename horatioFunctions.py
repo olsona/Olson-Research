9f577337814af74a4719027e83f983e9634cbee5
Anna Olson <olsona@cs.uchicago.edu>
2014-07-08 16:27:32 -0500

    more testing

--- horatioFunctions.py
+++ horatioFunctions.py
@@ -36,14 +36,13 @@ def scoreTETRA(DB, fSeed, matches, toMatch, allContigs):
 	mDB = "{!s}_M".format(DB)
 	os.system("perl tetraZscores.pl -k 4 -m {!s}-2 {!s} >/dev/null".format(fSeed,DB))
 	os.system("perl tetraZscores.pl -k 4 -m {!s}-2 {!s} >/dev/null".format(toMatch,mDB))
-	os.system("head -1 {!s}".format(DB))
-	os.system("head -1 {!s}".format(mDB))
 	os.system("perl tetraCorrelation.pl {!s} {!s} {!s} >/dev/null".format(DB,mDB,matches))
 	#os.system("rm {!s}".format(mDB))
 	
 	
 def scoreTETRAFinal(DB, fSeed, outputFile):
-    os.system("perl tetraZscores.pl -k 4 -m {!s} {!s} >/dev/null".format(fSeed,DB))
+    os.system("perl tetraZscores.pl -k 4 -m {!s}-2 {!s} >/dev/null".format(fSeed,DB))
+	
     os.system("perl tetraCorrelation.pl {!s} {!s} {!s}_dists_sorted >/dev/null".format(DB, DB, outputFile))
 	
 	
@@ -75,5 +74,5 @@ def scoreTACOA(DB, fSeed, matches, toMatch, allContigs):
 	
 	
 def scoreTACOAFinal(DB, fSeed, outputFile):
-	os.system("perl tacoaCount.pl -k 4 {!s} {!s} >/dev/null".format(fSeed,DB))
+	os.system("perl tacoaCount.pl -k 4 {!s}-2 {!s} >/dev/null".format(fSeed,DB))
 	os.system("perl tacoaDistance.pl {!s} {!s} {!s}_dists_sorted >/dev/null".format(DB, DB, outputFile))
