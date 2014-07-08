3bb79aa80cea7b45ae4031a88ad5fbec29a96876
Anna Olson <olsona@cs.uchicago.edu>
2014-07-07 22:38:03 -0500

    whitespace issues

--- horatioFunctions.py
+++ horatioFunctions.py
@@ -22,8 +22,8 @@ def scoreRAIphyFinal(DB, fSeed, toMatch, computePath, outputFile):
     os.system("{!s}rait -new -i {!s} -o {!s} >/dev/null 2>&1".format(computePath, fSeed, DB))
     os.system("{!s}rai -I {!s} -d {!s} >/dev/null 2>&1".format(computePath, toMatch, DB))
     short = toMatch.rsplit("/",1)[1]
-    os.system("cp {!s}/{!s}.bin {!s}".format(os.getcwd(), short, outputFile+"_dists_sorted")) # moves results to results folder
-    os.system("rm {!s}/{!s}.bin".format(os.getcwd(), short))
+    #os.system("cp {!s}/{!s}.bin {!s}".format(os.getcwd(), short, outputFile+"_dists_sorted")) # moves results to results folder
+    #os.system("rm {!s}/{!s}.bin".format(os.getcwd(), short))
 	
 	
 def scoreTETRA(DB, fSeed, matches, toMatch, allContigs):
