ff5f6475c9be232d42a9520492fdfa264741c4f4
Anna Olson <olsona@cs.uchicago.edu>
2014-08-07 22:07:17 -0500

    debugging forever

--- horatioFunctions.py
+++ horatioFunctions.py
@@ -38,9 +38,9 @@ def scoreTETRA(DB, fSeed, matches, toMatch, allContigs):
 	mDB = "{!s}_M".format(DB)
 	print "fSeed: " + str(os.path.getsize(fSeed+"-2"))
 	print "toMatch: " + str(os.path.getsize(toMatch+"-2"))
-	os.system("perl tetraZscores.pl -k 4 -m {!s}-2 {!s} ".format(fSeed,DB)) #>/dev/null
-	os.system("perl tetraZscores.pl -k 4 -m {!s}-2 {!s} ".format(toMatch,mDB))
+	os.system("perl tetraZscores.pl -k 4 -m {!s}-2 {!s} >/dev/null".format(fSeed,DB))
 	print "DB: "+str(os.path.getsize(DB))
+	os.system("perl tetraZscores.pl -k 4 -m {!s}-2 {!s} >/dev/null".format(toMatch,mDB))
 	print "mDB: "+str(os.path.getsize(mDB))
 	os.system("perl tetraCorrelation.pl {!s} {!s} {!s} >/dev/null".format(DB,mDB,matches))
 	#os.system("rm {!s}".format(mDB))
