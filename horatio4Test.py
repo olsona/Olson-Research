fd97bad4d72da3380580615ccd199768b08f7aaa
Anna Olson <olsona@cs.uchicago.edu>
2014-08-11 19:21:55 -0500

    debugging foreverrrrrrrrrrr

--- horatio4Test.py
+++ horatio4Test.py
@@ -42,31 +42,34 @@ def main(argv):
 	prefList = ['min','mean','max']
 	#for score in sorted(sDict.keys()):
 	sList = sDict[score]
-	for t in tDict:
-		for s in sList:
-			for j in jList:
-				for n in nList:
-					for pref in prefList:
-	#for t in ['4by4Chop']:
-	#	for s in [-17.0]:
-	#		for j in [0.5]:
-	#			for n in [0.02]:
-						splitList = [s]*(len(tDict[t])-1)
-						tStr = "["+",".join([str(ti) for ti in tDict[t]])+"]"
-						sStr = "["+",".join([str(si) for si in splitList])+"]"
-						print "{!s}, {!s}, {!s}, {!s}, {!s}, {!s}". format(score, n, j, tStr, sStr, pref)
-						myOut = "{!s}_{!s}_N_{!s}_J_{!s}_C_{!s}_L_{!s}_A_{!s}".format(out,score,n,j,t,s,pref)
-						fOut.write("{!s},{!s},{!s},{!s},{!s},{!s}".\
-							format(score, n, j, tStr, sStr, pref))
-						start = time.time()
-						os.system("python horatio.py -i {!s} -o {!s} -s {!s} -n {!s} -j {!s} -c {!s} -l {!s} -p {!s} -a {!s}".\
-							format(myfile, myOut, score, n, j, tStr, sStr, path, pref))
-						end = time.time()
-						fOut.write("{:03.2f}\n".format(end-start))
-						minutes = int((end-start)/60.0)
-						secs = int((end-start)-60.0*minutes)
-						print "{!s}:{!s}\n".format(minutes,str(secs).zfill(2))
-
+	
+	try:
+		for t in tDict:
+			for s in sList:
+				for j in jList:
+					for n in nList:
+						for pref in prefList:
+		#for t in ['4by4Chop']:
+		#	for s in [-17.0]:
+		#		for j in [0.5]:
+		#			for n in [0.02]:
+							splitList = [s]*(len(tDict[t])-1)
+							tStr = "["+",".join([str(ti) for ti in tDict[t]])+"]"
+							sStr = "["+",".join([str(si) for si in splitList])+"]"
+							print "{!s}, {!s}, {!s}, {!s}, {!s}, {!s}". format(score, n, j, tStr, sStr, pref)
+							myOut = "{!s}_{!s}_N_{!s}_J_{!s}_C_{!s}_L_{!s}_A_{!s}".format(out,score,n,j,t,s,pref)
+							fOut.write("{!s},{!s},{!s},{!s},{!s},{!s}".\
+								format(score, n, j, tStr, sStr, pref))
+							start = time.time()
+							os.system("python horatio.py -i {!s} -o {!s} -s {!s} -n {!s} -j {!s} -c {!s} -l {!s} -p {!s} -a {!s}".\
+								format(myfile, myOut, score, n, j, tStr, sStr, path, pref))
+							end = time.time()
+							fOut.write("{:03.2f}\n".format(end-start))
+							minutes = int((end-start)/60.0)
+							secs = int((end-start)-60.0*minutes)
+							print "{!s}:{!s}\n".format(minutes,str(secs).zfill(2))
+	except KeyboardInterrupt:
+		sys.exit()
 	fOut.close()			
 
 if __name__ == "__main__":
