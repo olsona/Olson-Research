b28510a93ee62e285b83cebb0875a23e0d604345
Anna Olson <olsona@cs.uchicago.edu>
2014-08-17 17:38:03 -0500

    sigh

--- horatio4Test.py
+++ horatio4Test.py
@@ -45,16 +45,16 @@ def main(argv):
 	sList = sDict[score]
 	
 	try:
-		#for t in tDict:			
-		#	for s in sList:
-		#		for j in jList:
-		#			for n in nList:
-		#				for pref in prefList:
-		for pref in ['median']:
-			for t in ['4allCloseChop']:
-				for s in [-17.0]:
-					for j in [0.5]:
-						for n in [0.02]:
+		for t in tDict:			
+			for s in sList:
+				for j in jList:
+					for n in nList:
+						for pref in prefList:
+		#for pref in ['median']:
+		#	for t in ['4allCloseChop']:
+		#		for s in [-17.0]:
+		#			for j in [0.5]:
+		#				for n in [0.02]:
 							splitList = [s]*(len(tDict[t])-1)
 							tStr = "["+",".join([str(ti) for ti in tDict[t]])+"]"
 							sStr = "["+",".join([str(si) for si in splitList])+"]"
