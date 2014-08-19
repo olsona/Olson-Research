fbdc2bec4c01c92ad6bc611f88ab555d0de58efd
Anna Olson <olsona@cs.uchicago.edu>
2014-08-19 13:44:23 -0500

    goddammit

--- horatio4Test.py
+++ horatio4Test.py
@@ -63,7 +63,7 @@ def main(argv):
 							splitList = [s]*(len(tDict[t])-1)
 							tStr = "["+",".join([str(ti) for ti in tDict[t]])+"]"
 							sStr = "["+",".join([str(si) for si in splitList])+"]"
-							print "{!s}, {!s}, {!s}, {!s}, {!s}, {!s}". format(score, n, j, tStr, sStr)
+							print "{!s}, {!s}, {!s}, {!s}, {!s}". format(score, n, j, tStr, sStr)
 							myOut = "{!s}_{!s}_N_{!s}_J_{!s}_C_{!s}_L_{!s}".format(out,score,n,j,t,s)
 							os.system('echo "::{!s},{!s},{!s},{!s},{!s}" >> {!s}'.\
 								format(score, n, j, tStr, sStr, outlog))
