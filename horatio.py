3abe2c9e0b85c56cbdc6f0d3b9719ee6c429646b
Anna Olson <olsona@cs.uchicago.edu>
2014-09-18 15:07:16 -0500

    SIGH

--- horatio.py
+++ horatio.py
@@ -500,8 +500,8 @@ def main(argv):
 	os.system("rm {!s} >/dev/null 2>&1".format(fSeed))
 	for i in range(leng+1):
 		os.system("rm {!s}_{!s}* >/dev/null 2>&1".format(baseName,i))
-	os.system("rm {!s} >/dev/null 2>&1".format(finalDists))
 	os.system("rm {!s}_dists_sorted >/dev/null 2>&1".format(outputFile))
+	os.system("rm {!s}_actualclusters*".format(outputFile))
 	
 	#distLog.close()
 
