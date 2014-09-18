92b3cf7f31b0e582e13199acbf6e7a72e0718296
Anna Olson <olsona@cs.uchicago.edu>
2014-09-18 11:51:42 -0500

    phew

---
+++ processSeedFile.pl
@@ -0,0 +1,18 @@
+#!/usr/bin/perl
+my ($path, $input, $output) = @ARGV;
+open(IN, $input);
+open(OUT1, '>', "$output-1");
+open(OUT2, '>', "$output-2");
+while (<IN>) {
+    chomp;
+    ($name, $seq) = split("\t");
+    $nname = substr $name, 1;
+    open(FI, '>', "$path$nname.fna");
+    print FI "$name\n$seq";
+    close(FI);
+    print OUT1 "$path$nname.fna\n";
+    print OUT2 "$nname\t$path$nname.fna\n";
+}
+close(IN);
+close(OUT1);
+close(OUT2);
